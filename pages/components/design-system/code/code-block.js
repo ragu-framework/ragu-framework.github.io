dea5988e6c2a6adef42bafa56a443ad4425f95ac
Maniero <cmaniero@thoughtworks.com>
2020-09-30 23:37:04 -0300

    adds the first info section: Using a micro-frontend

---
+++ pages/components/design-system/code/code-block.js
@@ -0,0 +1,21 @@
+class CodeBlock extends StyleSlot {
+  get styleTemplate() {
+    return `
+      <style>
+        :host {
+          background: #2B3332;
+          color: #FFFFFF;
+          padding: 1.5rem 0.75rem;
+          font-family: 'Source Code Pro', monospace;
+          overflow: auto;
+          display: block;
+          box-sizing: border-box;
+          max-width: 100%;
+          white-space: nowrap;
+        }
+      </style>
+    `
+  }
+}
+
+customElements.define('code-block', CodeBlock);
