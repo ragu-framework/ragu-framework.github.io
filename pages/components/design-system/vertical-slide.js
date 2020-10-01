77d9b489ddb8f138995eb23eeb5d2bea593c085c
Maniero <cmaniero@thoughtworks.com>
2020-10-01 00:34:19 -0300

    adds why to use section

---
+++ pages/components/design-system/vertical-slide.js
@@ -0,0 +1,35 @@
+class VerticalSlide extends StyleSlot {
+  template = `
+    <div class="scroller">
+      <slot></slot>
+    </div> 
+  `
+
+  get styleTemplate() {
+    return `
+      <style>
+        :host {
+          width: calc(100% + 40px);
+          box-sizing: border-box;
+          margin-left: -20px;
+          display: block;
+        }
+        .scroller {
+          display: flex;
+          overflow: auto;
+        }
+        .scroller ::slotted(*) {
+          min-width: 240px;
+          flex-basis: auto;
+          flex-grow: 1;
+          margin-left: 40px;
+        }
+        .scroller ::slotted(*:first-child) {
+         margin-left: 20px;
+        }
+      </style>
+    `;
+  }
+}
+
+customElements.define('vertical-slide', VerticalSlide);
