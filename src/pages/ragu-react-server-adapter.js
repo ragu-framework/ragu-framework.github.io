26ed5040c4122c3c340e87c0b550448b7d7b3223
Maniero <carlosmaniero@gmail.com>
2021-02-26 13:37:01 -0300

    uses a static react docs for a better performance

--- src/pages/ragu-react-server-adapter.js
+++ src/pages/ragu-react-server-adapter.js
@@ -11,7 +11,7 @@ export class RaguReactServerAdapter extends StyleSlot {
   get template() {
     return `
     ${MainContent.render(`
-      ${MfLoader.render('', {src: 'https://ragu-react-server-adapter.herokuapp.com/components/installation'})}
+      ${MfLoader.render('', {src: 'https://ragu-framework.github.io/ragu-react-server-adapter/index.json'})}
     `)}
     `
   }
